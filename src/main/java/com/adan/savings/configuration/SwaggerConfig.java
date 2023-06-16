b0a5e2557730e44301fc25b2f751fa0dcf9ff430
Adan Ali <aadan4762@gmail.com>
2023-06-16 21:05:44 +0300

    Update SwaggerConfig.java

--- src/main/java/com/adan/savings/configuration/SwaggerConfig.java
+++ src/main/java/com/adan/savings/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.adan.savings.configuration;
+/**package com.adan.savings.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,4 +33,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-}
+}**/
